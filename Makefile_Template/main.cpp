5623aa5ae9d8d4c05d8da467ce1155fd76ec9c1d
Alexander Ulbrich <alexander.ulbrich@uni-ulm.de>
2017-09-08 10:14:31 +0200

    some problem with i2c happend. Don't know if its hardware or software

--- Makefile_Template/main.cpp
+++ Makefile_Template/main.cpp
@@ -2,14 +2,24 @@
 **/
 
 #include "Goldboard4.h"
+#include "lcd.h"
 
 Goldboard4 gb;
 
 int main(void)
 {	
+	gb.initLED(1);
+	gb.setLed(1,0);
+	//lcd_init(LCD_DISP_ON_CURSOR_BLINK,&gb.digital);
+	//lcd_puts("hello world");
 	while(1)
 	{
-		
+		gb.motor[1].rotate(100);
+		delay(100);
+		gb.setLed(1,0);
+		gb.motor[1].rotate(-100);
+		delay(100);
+		gb.setLed(1,1);
 	}
 }
 
