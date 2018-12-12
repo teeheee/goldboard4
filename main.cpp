b0c04211b53c1191759f0bbbc03b0c2f1ffafe45
Alexander Ulbrich <alexander.ulbrich@uni-ulm.de>
2018-12-12 14:21:28 +0100

    test main

--- main.cpp
+++ main.cpp
@@ -1,11 +1,21 @@
 #include "Goldboard4.h"
+#include "LSM303.h"
 
 Goldboard4 gb;
 
 int main(){
+	LSM303 lsm;
+
+ 	lsm.init();
+  	lsm.enableDefault();
+
+  	lsm.m_min = (LSM303::vector<int16_t>){-32767, -32767, -32767};
+	lsm.m_max = (LSM303::vector<int16_t>){+32767, +32767, +32767};
 	while(1)
 	{
-
+		lsm.read();
+		SERIAL_PRINTLN(lsm.heading());
+		delay(500);
 	}
 	return 0;
 }
